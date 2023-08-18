881e2216efacd25186e7396a9b43de8c4ac76c8f
leejss <goldemshine@gmail.com>
2023-08-19 00:46:58 +0900

    add queue

---
+++ src/Queue/Queue.test.ts
@@ -0,0 +1,46 @@
+import { describe, it, expect, test, beforeEach } from "vitest";
+import Queue from "./Queue";
+
+describe("Queue<T>", () => {
+  let queue: Queue<number>;
+
+  beforeEach(() => {
+    queue = new Queue<number>();
+  });
+
+  test("should initialize as empty", () => {
+    expect(queue.isEmpty()).toBe(true);
+  });
+
+  test("should enqueue items", () => {
+    queue.enqueue(1);
+    expect(queue.peek()).toBe(1);
+    expect(queue.size()).toBe(1);
+  });
+
+  test("should dequeue items", () => {
+    queue.enqueue(1);
+    queue.enqueue(2);
+    expect(queue.dequeue()).toBe(1);
+    expect(queue.peek()).toBe(2);
+    expect(queue.size()).toBe(1);
+  });
+
+  test("should return null when dequeueing from an empty queue", () => {
+    expect(queue.dequeue()).toBeNull();
+  });
+
+  test("should return null when peeking an empty queue", () => {
+    expect(queue.peek()).toBeNull();
+  });
+
+  test("should return the correct size", () => {
+    expect(queue.size()).toBe(0);
+    queue.enqueue(1);
+    expect(queue.size()).toBe(1);
+    queue.enqueue(2);
+    expect(queue.size()).toBe(2);
+    queue.dequeue();
+    expect(queue.size()).toBe(1);
+  });
+});
---
+++ src/Queue/Queue.ts
@@ -0,0 +1,26 @@
+export default class Queue<T> {
+  private array: T[] = [];
+
+  isEmpty = () => {
+    return this.array.length === 0;
+  };
+
+  enqueue = (item: T) => {
+    this.array.push(item);
+  };
+  dequeue = (): T | null => {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.array.shift() as T;
+  };
+  peek = () => {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.array[0];
+  };
+  size = () => {
+    return this.array.length;
+  };
+}
