7d271d19f8b52241bdb7b5d32bfcc17a8484621a
leejss <goldemshine@gmail.com>
2023-08-19 13:55:23 +0900

    chore: add new script

---
+++ scripts/new.sh
@@ -0,0 +1,33 @@
+
+base_dir=./src/
+name=$1
+
+
+
+if [ -z $name ]; then
+    echo "Usage: $0 <name>"
+    exit 1
+fi
+
+
+path="$base_dir/$name"
+
+if [ -d $path ]; then
+    echo "Error: $path already exists"
+    exit 1
+fi
+
+
+mkdir $path
+cd $path
+
+# Name.ts
+echo "export default class ${name} {}" > ${name}.ts
+
+# Name.test.ts
+echo "import ${name} from \"./${name}\"; 
+import { describe, beforeEach, test, expect, vi } from \"vitest\";" > ${name}.test.ts
+
+
+# index.ts
+echo "export { default as ${name} } from \"./${name}\"" > index.ts
