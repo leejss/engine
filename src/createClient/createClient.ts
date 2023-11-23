364805e67dd69807c344ba2bfd7ab94fd40998c2
leejss <goldemshine@gmail.com>
2023-11-23 23:50:17 +0900

    feat: add configurable client

---
+++ src/createClient/createClient.ts
@@ -0,0 +1,28 @@
+// GOAL: Make configurable and extendable client
+import ky, { KyInstance, Options } from "ky";
+
+type CustomKyConfig = {
+  baseURL: string;
+  defaultHeaders: HeadersInit;
+};
+
+type ClientConfig = { name: string } & CustomKyConfig;
+
+const createKyInstance = (config: CustomKyConfig): KyInstance =>
+  ky.create({
+    prefixUrl: config.baseURL,
+    headers: config.defaultHeaders,
+  });
+
+// Configurable and extendable client
+export const createBaseClient = ({ name, ...config }: ClientConfig) => {
+  const kyInstance = createKyInstance(config);
+  return {
+    getClient: () => kyInstance,
+    // Set function will change the existing ky instance
+    set: (opt: Options) => {
+      kyInstance.extend(opt);
+    },
+    extend: (opt: Options & { name: string }) => createBaseClient({ ...config, ...opt }),
+  };
+};
